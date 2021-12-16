21991fc40027ed10fa86a4c843f9cf35a65abe3e
vipzero <hiroflighter@gmail.com>
2021-12-16 12:26:46 +0900

    fix aa

--- exported-git_2021.sh
+++ exported-git_2021.sh
@@ -8,39 +8,39 @@ git commit --date="Tue Feb  2 12:00:00 2021 +0900" -m "yes"
 touch 2021/2021-02-16
 git add --all
 git commit --date="Tue Feb 16 12:00:00 2021 +0900" -m "yes"
-touch 2021/2021-02-26
+touch 2021/2021-02-19
 git add --all
-git commit --date="Fri Feb 26 12:00:00 2021 +0900" -m "yes"
-touch 2021/2021-03-04
+git commit --date="Fri Feb 19 12:00:00 2021 +0900" -m "yes"
+touch 2021/2021-02-25
 git add --all
-git commit --date="Thu Mar  4 12:00:00 2021 +0900" -m "yes"
+git commit --date="Thu Feb 25 12:00:00 2021 +0900" -m "yes"
+touch 2021/2021-02-27
+git add --all
+git commit --date="Sat Feb 27 12:00:00 2021 +0900" -m "yes"
 touch 2021/2021-03-06
 git add --all
 git commit --date="Sat Mar  6 12:00:00 2021 +0900" -m "yes"
-touch 2021/2021-03-13
+touch 2021/2021-03-12
 git add --all
-git commit --date="Sat Mar 13 12:00:00 2021 +0900" -m "yes"
-touch 2021/2021-03-19
+git commit --date="Fri Mar 12 12:00:00 2021 +0900" -m "yes"
+touch 2021/2021-03-20
 git add --all
-git commit --date="Fri Mar 19 12:00:00 2021 +0900" -m "yes"
+git commit --date="Sat Mar 20 12:00:00 2021 +0900" -m "yes"
+touch 2021/2021-03-25
+git add --all
+git commit --date="Thu Mar 25 12:00:00 2021 +0900" -m "yes"
 touch 2021/2021-03-27
 git add --all
 git commit --date="Sat Mar 27 12:00:00 2021 +0900" -m "yes"
-touch 2021/2021-04-01
+touch 2021/2021-03-29
 git add --all
-git commit --date="Thu Apr  1 12:00:00 2021 +0900" -m "yes"
-touch 2021/2021-04-03
+git commit --date="Mon Mar 29 12:00:00 2021 +0900" -m "yes"
+touch 2021/2021-03-30
 git add --all
-git commit --date="Sat Apr  3 12:00:00 2021 +0900" -m "yes"
-touch 2021/2021-04-05
+git commit --date="Tue Mar 30 12:00:00 2021 +0900" -m "yes"
+touch 2021/2021-04-02
 git add --all
-git commit --date="Mon Apr  5 12:00:00 2021 +0900" -m "yes"
-touch 2021/2021-04-06
+git commit --date="Fri Apr  2 12:00:00 2021 +0900" -m "yes"
+touch 2021/2021-04-13
 git add --all
-git commit --date="Tue Apr  6 12:00:00 2021 +0900" -m "yes"
-touch 2021/2021-04-09
-git add --all
-git commit --date="Fri Apr  9 12:00:00 2021 +0900" -m "yes"
-touch 2021/2021-04-20
-git add --all
-git commit --date="Tue Apr 20 12:00:00 2021 +0900" -m "yes"
+git commit --date="Tue Apr 13 12:00:00 2021 +0900" -m "yes"
